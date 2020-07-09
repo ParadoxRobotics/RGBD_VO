4fab9430d838f25b63715899a2743e861da1f46f
ParadoxRobotics <45688885+ParadoxRobotics@users.noreply.github.com>
2020-07-09 18:49:35 +0200

    Update RGBDVO.py

--- RGBDVO.py
+++ RGBDVO.py
@@ -1,6 +1,19 @@
 # NumPy/Python RGBD Visual Odometry for robotics applications:
 # Author :  MUNCH Quentin 2018/2019
 
+"""
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
 import cv2
