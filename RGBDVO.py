a24c10884b9c17e0fad1c9e40cb4e205cd8d39ab
ParadoxRobotics <45688885+ParadoxRobotics@users.noreply.github.com>
2023-09-11 09:25:40 +0200

    Update RGBDVO.py

--- RGBDVO.py
+++ RGBDVO.py
@@ -23,34 +23,6 @@ from matplotlib import pyplot as plt
 from matplotlib import pyplot
 import icp
 
-# use ggplot style for more sophisticated visuals
-plt.style.use("ggplot")
-
-
-def live_plotter(x_vec, y1_data, line1, identifier="", pause_time=0.1):
-    if line1 == []:
-        # this is the call to matplotlib that allows dynamic plotting
-        plt.ion()
-        fig = plt.figure(figsize=(13, 6))
-        ax = fig.add_subplot(111)
-        # create a variable for the line so we can later update it
-        (line1,) = ax.plot(x_vec, y1_data, "-o", alpha=0.8)
-        # update plot label/title
-        plt.ylabel("Y Label")
-        plt.title("Title: {}".format(identifier))
-        plt.show()
-
-    # after the figure, axis, and line are created, we only need to update the y-data
-    line1.set_ydata(y1_data)
-    # adjust limits if new data goes beyond bounds
-    if np.min(y1_data) <= line1.axes.get_ylim()[0] or np.max(y1_data) >= line1.axes.get_ylim()[1]:
-        plt.ylim([np.min(y1_data) - np.std(y1_data), np.max(y1_data) + np.std(y1_data)])
-    # this pauses the data so the figure/axis can catch up - the amount of pause can be altered above
-    plt.pause(pause_time)
-
-    # return line so we can update it again in the next iteration
-    return line1
-
 
 Rot_pose = np.eye(3)
 Tr_pose = np.array([[0], [0], [0]])
